6829c6bb9bf379f2d7a24a9fdd2d0c5b275ac28e
Chris Block <dashonice@gmail.com>
2015-08-02 16:53:00 -0700

    name change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
+        <title>Brand New Awesome</title>
 
         <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Brand New <strong>Awesome</strong></div>
             </div>
         </div>
     </body>
